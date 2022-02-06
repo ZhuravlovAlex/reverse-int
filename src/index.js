71627a63becdeb589eecd49ed88328a4d269f2e3
ZhuravlovAlex <zhuravlov.alex75@gmail.com>
2022-02-06 18:32:57 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let string = n.toString();
+  let array = string.split("");
+  let arrayReverse = array.reverse();
+  let newString = arrayReverse.join("");
+ 
+ 
+  let nReverse = parseInt(newString, 10);
+  return nReverse
 }
